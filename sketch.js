79d9d1faa3bbadadb901f6ac4ab05d9bcbd8fe2f
sisko81 <abdurcampbell@yahoo.com>
2022-09-21 08:08:32 +0900

    update

---
+++ sketch.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+container.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+for(let i = 0; i < 256; i++) {
+    const square = document.createElement('div')
+}
