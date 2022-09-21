0f43772666fcc3ca7f5ec3b47ad473db511901e6
sisko81 <abdurcampbell@yahoo.com>
2022-09-21 20:36:14 +0900

    Created a 16x16 grid inside my container

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+
+
 const container = document.querySelector('#container');
 container.style.gridTemplateColumns = 'repeat(16, 1fr)';
 container.style.gridTemplateRows = 'repeat(16, 1fr)';
+//container.style.border = '1px solid blue'
 
 for(let i = 0; i < 256; i++) {
-    const square = document.createElement('div')
+    const square = document.createElement('div');
+    square.style.backgroundColor = 'blue'
+
+    container.insertAdjacentElement('beforeend', square);
+
 }
