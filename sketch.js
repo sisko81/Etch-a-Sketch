fbe3ee523a97d2121a2002dab9f5bc9ca4840efd
sisko81 <abdurcampbell@yahoo.com>
2022-09-25 09:19:01 +0900

    Rewrote first step to cleaner arrow function

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
+const grid = (size) => {
+    const container = document.querySelector('#container');
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
-const container = document.querySelector('#container');
-container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-container.style.gridTemplateRows = 'repeat(16, 1fr)';
-//container.style.border = '1px solid blue'
-
-for(let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.style.backgroundColor = 'blue'
-
-    container.insertAdjacentElement('beforeend', square);
-
+    let = playArea = size * size;
+    for(let i = 0; i < playArea; i++) {
+        const square = document.createElement('div');
+        square.id = ('box');
+        square.style.backgroundColor = 'blue';
+        container.appendChild(square)
+    }
 }
+
+grid(7)
